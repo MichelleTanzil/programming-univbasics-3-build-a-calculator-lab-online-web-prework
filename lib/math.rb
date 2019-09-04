b3bbf1aaba31e39a95b203dc8300eb7842ae7da9
Michelle Tanzil <mtanzil01@gmail.com>
2019-09-04 21:03:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  num2 - num1
+  subtraction = num2 - num1
+  return subtraction
 end
 
 def division(num1, num2)
-  
+  division = num2 * num1
+  return division
 end
 
 def multiplication(num1, num2)
-  num1 * num2 
+  multiplication = num1 * num2 
+  return multiplication
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
-
+  num2 = num ** 2 
+  return num2
 end
