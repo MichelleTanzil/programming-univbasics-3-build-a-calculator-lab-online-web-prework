fdd899d3d5c38a3436fd64f2f6f6459265637c1b
Michelle Tanzil <mtanzil01@gmail.com>
2019-09-04 21:01:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,6 @@
 def addition(num1, num2)
-  num1 + num2
+ addition = num1 + num2
+ return addition
 end
 
 def subtraction(num1, num2)
@@ -11,7 +12,7 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2 
 end
 
 def modulo(num1, num2)
